addf5e52ed54ffc66aef86ef36707915c8c002e2
kinyarasam <skinyara.30@gmail.com>
2026-09-04 14:14:47 +0300

    test: expand move validator coverage

--- tests/domain/moves/MoveValidator.test.ts
+++ tests/domain/moves/MoveValidator.test.ts
@@ -8,6 +8,7 @@ import { MoveValidator } from '../../../src/domain/moves/MoveValidator.js';
 import { Color } from '../../../src/domain/pieces/Color.js';
 import { Piece } from '../../../src/domain/pieces/Piece.js';
 import { PieceType } from '../../../src/domain/pieces/PieceType.js';
+import { MoveType } from '../../../src/domain/moves/MoveType.js';
 
 describe('MoveValidator', () => {
   it('accepts a generated pawn move', () => {
@@ -87,5 +88,66 @@ describe('MoveValidator', () => {
     const validator = new MoveValidator();
 
     expect(validator.isLegal(position, move)).toBe(false);
+  });
+
+  it('rejects a king move onto an attacked square', () => {
+    const board = new Board();
+
+    board.setPiece(
+      Square.fromAlgebraic('e1'),
+      new Piece(Color.White, PieceType.King),
+    );
+
+    board.setPiece(
+      Square.fromAlgebraic('a8'),
+      new Piece(Color.Black, PieceType.King),
+    );
+
+    board.setPiece(
+      Square.fromAlgebraic('e8'),
+      new Piece(Color.Black, PieceType.Rook),
+    );
+
+    const position = new Position(board, Color.White);
+
+    const move = new Move(
+      Square.fromAlgebraic('e1'),
+      Square.fromAlgebraic('e2'),
+    );
+
+    const validator = new MoveValidator();
+
+    expect(validator.isLegal(position, move)).toBe(false);
+  });
+
+  it('accepts a king capture when the destination square is safe', () => {
+    const board = new Board();
+
+    board.setPiece(
+      Square.fromAlgebraic('e1'),
+      new Piece(Color.White, PieceType.King),
+    );
+
+    board.setPiece(
+      Square.fromAlgebraic('a8'),
+      new Piece(Color.Black, PieceType.King),
+    );
+
+    board.setPiece(
+      Square.fromAlgebraic('e2'),
+      new Piece(Color.Black, PieceType.Rook),
+    );
+
+    const position = new Position(board, Color.White);
+
+    const move = new Move(
+      Square.fromAlgebraic('e1'),
+      Square.fromAlgebraic('e2'),
+      MoveType.Capture,
+    );
+
+    const validator = new MoveValidator();
+
+    expect(validator.isLegal(position, move)).toBe(true);
   });
 });
